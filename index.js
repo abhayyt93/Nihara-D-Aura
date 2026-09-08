ba96beff5f2e8deab570dba4b0f070cf6c7a1173
abhayyt93 <abhayyt93@gmail.com>
2026-09-08 17:08:24 +0530

    Change get request

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Kosmatic Backend is running!');
+  res.send('Nihara D Aura backend is running!');
 });
 
 app.listen(PORT, () => {
